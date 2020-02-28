c5b3af58aa990c85ef4da74f4cf3863aacda6f4f
NasirShaik <nasir4urfrnd@gmail.com>
2020-02-28 18:18:49 +0530

    second commit

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-const expressLayouts = require('express-ejs-layouts')
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 //Load env vars
 dotenv.config({path:'./config/config.env'});
@@ -12,8 +13,9 @@ const app = express();
 mongoose.connect(process.env.DB_ENV,{useNewUrlParser:true, useUnifiedTopology: true})
 .then(()=>{console.log('successfully Connected DB')})
 .catch((err)=>{console.log('db not connected',err)})
-
+//routes imports
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/author');
 
 app.set('view engine', 'ejs');
 app.set('views',__dirname +'/views');
@@ -22,7 +24,11 @@ app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 const PORT = process.env.PORT || 3000;
 
